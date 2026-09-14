4855337229b3554a9b4f80d03ac544ed1dfdea1c
Azim <azizbekovv.28@gmail.com>
2026-09-14 21:25:08 +0600

    lesson5

---
+++ Lesson_5.1.py
@@ -0,0 +1,16 @@
+#Множественное наследование
+
+class Flying:
+    def fly(self):
+        print("Лечу")
+
+class Swimming:
+    def swim(self):
+        print("Плыву")
+
+class Duck(Flying, Swimming):
+    pass
+
+donald = Duck()
+donald.fly()
+donald.swim()
