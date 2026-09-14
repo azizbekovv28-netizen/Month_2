fcb5eeecfeff58f058024ce5d20932e359a13505
Azim <azizbekovv.28@gmail.com>
2026-09-14 21:25:19 +0600

    lesson4

--- Lesson_4.2.py
+++ Lesson_4.2.py
@@ -16,6 +16,12 @@ class Visitor:
     def get_visitor_count(cls):
         return cls.count
 
+    @classmethod
+    def from_text(cls, text):
+        text = text.strip()
+        new_visitor =  Visitor(text)
+        return new_visitor
+
     @staticmethod
     def validate_name(name):
         if name.strip():
@@ -27,4 +33,7 @@ visitor2 = Visitor("Алмаз")
 Visitor.organisation = "Об.Ц. Geeks"
 print(Visitor.organisation, Visitor.count)
 print(Visitor.get_visitor_count())
-print(Visitor.validate_name(""))
+print(Visitor.validate_name("      "))
+visitor3 = Visitor.from_text("         Мурат      ")
+print(visitor3.name, Visitor.get_visitor_count())
+Visitor3 = Visitor.from_text("           ")
