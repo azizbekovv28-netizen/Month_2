36d6369fe37bce8aaf8e3f2fed8496378b4c3fa5
Azim <azizbekovv.28@gmail.com>
2026-09-14 21:25:39 +0600

    homework5

---
+++ homework_5.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+
+class File(ABC):
+    def open (self):
+        pass
+
+    def get_file_info(self):
+        pass
+
+class TextFile(File):
+    def open (self):
+        print("Открываем текстовый файл:")
+        print("Ассалам Алейкум, меня зовут Азим")
+
+    def get_file_info(self):
+        print("Тип файла: Текст")
+
+class ImageFile(File):
+    def open (self):
+        print("Открываем изображение:")
+        print("------------>>>>>>><<<<<<<<______________")
+    def get_file_info(self):
+        print("Тип файла: Изображение")
+
+class AudioFile(File):
+    def open (self):
+        print("Воспроизводим аудиозапись: ")
+
+    def get_file_info(self):
+        print("Тип файла: Аудио")
+
+class VideoFile(File):
+    def open (self):
+        print("Воспроизводим видеозапись: ")
+
+    def get_file_info(self):
+        print("Тип файла: Видео")
+
+class ArhiveFile(File):
+    def open (self):
+        print("Открываем архив:")
+
+files = [
+    TextFile(),
+    ImageFile(),
+    AudioFile(),
+    VideoFile(),
+    ArhiveFile(),
+]
+
+for file in files:
+    file.open()
+    file.get_file_info()
+    print()
+
+
