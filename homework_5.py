42a48952b233b600b22edf5afcbf0959764df270
Azim <azizbekovv.28@gmail.com>
2026-09-20 18:20:02 +0600

    homework5 исправил ошибки

--- homework_5.py
+++ homework_5.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class File(ABC):
     @abstractmethod
-    def open (self):
+    def open(self):
         pass
 
     @abstractmethod
@@ -10,7 +10,7 @@ class File(ABC):
         pass
 
 class TextFile(File):
-    def open (self):
+    def open(self):
         print("Открываем текстовый файл:")
         print("Ассалам Алейкум, меня зовут Азим")
 
@@ -18,28 +18,28 @@ class TextFile(File):
         print("Тип файла: Текст")
 
 class ImageFile(File):
-    def open (self):
+    def open(self):
         print("Открываем изображение:")
         print("------------>>>>>>><<<<<<<<______________")
     def get_file_info(self):
         print("Тип файла: Изображение")
 
 class AudioFile(File):
-    def open (self):
+    def open(self):
         print("Воспроизводим аудиозапись: ")
 
     def get_file_info(self):
         print("Тип файла: Аудио")
 
 class VideoFile(File):
-    def open (self):
+    def open(self):
         print("Воспроизводим видеозапись: ")
 
     def get_file_info(self):
         print("Тип файла: Видео")
 
 class ArhiveFile(File):
-    def open (self):
+    def open(self):
         print("Открываем архив:")
 
 files = [
@@ -60,7 +60,7 @@ except TypeError as e:
     print("Ошибка")
 
 try:
-    arhive = ArhiveFile()
+    arсhive = ArhiveFile()
 except TypeError as e:
     print("Ошибка")
 
